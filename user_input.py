3aa23f0d7c803901c62ded3e294f0f0b2b79e135
Nalin1_dev <kevnal456@gmail.com>
2024-11-10 11:18:43 +0300

    introduction to python

---
+++ user_input.py
@@ -0,0 +1,9 @@
+
+
+# Ask the user for their name, age, and location
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+location = input("Please enter your location: ")
+
+# Print a personalized message
+print("Hello $name and you are $age years old and live in $location.")
